dcf7eb9f5a40db2c812bb922fbead2eb0a96e378
SammySatoro <ivzhenko2014@gmail.com>
2023-10-10 03:08:23 +0300

    testing pyswip

--- file.pl
+++ file.pl
@@ -1,41 +1,37 @@
-:- use_module(library(random)).
+:- dynamic matrix/1.
+:- dynamic(cells/1).
 
-% Define the size of the matrix (10x10)
-size(10).
 
-% Generate a random permutation of cells
-random_permutation_cells(Permutation) :-
-    size(Size),
-    findall(X-Y, (between(1, Size, X), between(1, Size, Y)), Cells),
-    random_permutation(Cells, Permutation).
+matrix.
 
-% Define the ship lengths
-ship_lengths([4, 3, 3, 2, 2, 2, 20, 20, 20, 20]).
+unknown.    %0
+damaged.    %1
+broken.     %2
+possible.   %3
+destroyed.  %4
 
-% Place ships randomly on the matrix
-place_ships(Matrix) :-
-    random_permutation_cells(Permutation),
-    ship_lengths(ShipLengths),
-    place_ships(ShipLengths, Permutation, Matrix).
 
-place_ships([], _, _).
-place_ships([Length|Rest], [X-Y|RestCoords], Matrix) :-
-    set_cell(Matrix, X, Y, 'B'),
-    assert(battleship(X, Y)), % Add the ship coordinates to facts
-    Length1 is Length - 1,
-    place_ship(Length1, RestCoords, Matrix).
 
-set_cell(Matrix, X, Y, Value) :-
-    nth1(X, Matrix, Row),
-    nth1(Y, Row, Value).
+random_cell(Result) :-
+    cells(Cells),
+    random_member(Cell, Cells),
+    Result = Cell.
 
-% Query to generate a matrix with ship placements
-generate_matrix(Matrix) :-
-    size(Size),
-    length(Matrix, Size),
-    generate_matrix_rows(Size, Matrix).
+remove_value(cells(List), Value) :-
+    select(Value, List, NewList),
+    retract(cells(_)),
+    assert(cells(NewList)).
 
-generate_matrix_rows(_, []).
-generate_matrix_rows(Size, [Row|Rest]) :-
-    length(Row, Size),
-    generate_matrix_rows(Size, Rest).
+% Define a helper predicate to update a single element
+update_element(Matrix, X, Y, Value, NewMatrix) :-
+    nth0(X, Matrix, Row, RestRows),
+    nth0(Y, Row, Element, RestRow),
+    NewElement is Element + Value,
+    nth0(X, NewRow, NewElement, RestRow),
+    nth0(Y, NewMatrix, NewRow, RestRows).
+
+% Define the main predicate to update the matrix
+update_matrix(Matrix, [X, Y], Value, NewMatrix) :-
+    update_element(Matrix, X, Y, Value, NewMatrix).
+
+
--- main.py
+++ main.py
@@ -12,25 +12,64 @@ def main():
     menu_window.show()
     sys.exit(app.exec())
 
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
 
 def test():
-    # Create a Prolog instance
-    prolog = Prolog()
+    # 0 - unknown
+    # 1 - damaged
+    # 2 - broken
+    # 3 - possible (around broken (NESW))
+    # 4 - destroyed
 
-    # Load the Prolog file
+    prolog = Prolog()
     prolog.consult("file.pl")
 
-    # Pass the matrix and K as arguments to the Prolog program and get the shifted matrix
-    query = "sum_of_digits(123454321, Sum)"
-    result = list(prolog.query(query))
+    matrix1 = [
+        [0, 1, 0, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
 
-    for i in result:
-        print(i["Sum"])
+    matrix2 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    cells = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [0, 1], [1, 1], [2, 1],
+        [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2],
+        [7, 2], [8, 2], [9, 2], [0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [0, 4],
+        [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [0, 5], [1, 5], [2, 5], [3, 5], [4, 5],
+        [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6], [8, 6],
+        [9, 6], [0, 7], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7], [8, 7], [9, 7], [0, 8], [1, 8], [2, 8],
+        [3, 8], [4, 8], [5, 8], [6, 8], [7, 8], [8, 8], [9, 8], [0, 9], [1, 9], [2, 9], [3, 9], [4, 9], [5, 9], [6, 9],
+        [7, 9], [8, 9], [9, 9]]
+
+    prolog.assertz(f"cells({cells})")
+    solution = list(prolog.query("random_cell(Cell)"))
+    print(solution)
+    solution_for_prolog = [solution[0]['Cell'][0], solution[0]['Cell'][1]]
+    list(prolog.query(f"remove_value(cells({cells}), {solution[0]['Cell']})"))
+    cells.remove(solution[0]['Cell'])
+
+    new_matrix = list(prolog.query(f"update_matrix({matrix2}, {solution_for_prolog}, 3, NewMatrix)"))[0]["NewMatrix"]
+    for row in new_matrix:
+        for item in row:
+            print(item, end=" ")
+        print()
+
+
+if __name__ == '__main__':
+    test()
+
+
+
+
+
+
+
